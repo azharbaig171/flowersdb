273c6db25a4db8eb20aea6a3191b86bbc81364bb
andrew-helms <andrew.helms@gmail.com>
2019-12-12 17:17:06 -0600

    ViewFlower Base code added

--- client/src/components/AddSighting.js
+++ client/src/components/AddSighting.js
@@ -2,21 +2,26 @@ import React from 'react';
 import axios from 'axios';
 
 class AddSighting extends React.Component  {
-
-render() {
-
-    return(
-        <div>
-            <p>testing</p>
-        </div>
-    )
-
-    
-
-
-
-
+    state = {
+        flowers: []
     }
+
+    componentDidMount() {
+        axios.get(`http://localhost:8080/api/flowers/`)
+            .then(res => {
+                const flowers = res.data.data;
+                this.setState({ flowers });
+        })
+    }
+
+	render() {
+
+		return(
+			<div>
+				<p>testing</p>
+			</div>
+		)
+	}
 
 }
 
--- client/src/components/ViewFlower.js
+++ client/src/components/ViewFlower.js
@@ -2,21 +2,31 @@ import React from 'react';
 import axios from 'axios';
 
 class ViewFlower extends React.Component  {
-
-render() {
-
-    return(
-        <div>
-            <p>testing</p>
-        </div>
-    )
-
-    
-
-
-
-
+    state = {
+        flowers: []
     }
+
+    componentDidMount() {
+		const flowerName = "Draperia"//this.props.name
+		
+        axios.get(`http://localhost:8080/api/flowers/${flowerName}/sightings`)
+            .then(res => {
+                const flowers = res.data.data;
+                this.setState({ flowers });
+        })
+    }
+	
+	render() {
+		console.log(this.state.flowers);
+
+		return(
+			<div>
+			{this.state.flowers.map(flower => (
+				<p>{flower.NAME}&emsp; {flower.PERSON}&emsp; {flower.LOCATION}&emsp; {flower.SIGHTED}</p>
+			))}
+			</div>
+		)
+	}
 
 }
 
